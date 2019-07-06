a28f5ed49d2c194410968dfa04ed640483d7f214
Philip Sumpton <philipsumpton@philips-mbp.home>
2019-07-06 15:30:20 +0100

    Added new view controller for selecting samples

---
+++ Simple Drum/DrumSelectorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DrumSelectorViewController.swift
+//  Simple Drum
+//
+//  Created by Philip Sumpton on 06/07/2019.
+//  Copyright © 2019 Philip Sumpton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DrumSelectorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
